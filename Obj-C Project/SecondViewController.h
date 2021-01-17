2f3f803446c456da76c19b8d217e107219c6de00
Dmitrii Diadiushkin <stephandvd@yandex.ru>
2021-01-17 20:35:08 +0500

    End

---
+++ Obj-C Project/SecondViewController.h
@@ -0,0 +1,16 @@
+//
+//  SecondViewController.h
+//  Obj-C Project
+//
+//  Created by Dmitrii Diadiushkin on 17.01.2021.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SecondViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Obj-C Project/SecondViewController.m
@@ -0,0 +1,31 @@
+//
+//  SecondViewController.m
+//  Obj-C Project
+//
+//  Created by Dmitrii Diadiushkin on 17.01.2021.
+//
+
+#import "SecondViewController.h"
+
+@interface SecondViewController ()
+
+@end
+
+@implementation SecondViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
